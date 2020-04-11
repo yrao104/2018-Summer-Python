4a7d04a7a5d5576f4e617d2c603fa54e629ddfa9
Yamuna Rao <63515445+yrao104@users.noreply.github.com>
2020-04-11 12:24:47 -0700

    Update and rename name to Exercise6.py

---
+++ Unit1/Exercise6.py
@@ -0,0 +1,9 @@
+#Exercise #6
+enteredName = input("What is your name?") 
+enteredAge = input("How old are you")
+print ("Hello, " + enteredName + ". " + enteredAge + " years old is the best age!")
+'''
+'''
+numb3 = input("enter numb3")
+numb4 = input("enter numb4")
+print(int(numb3) + int(numb4))
