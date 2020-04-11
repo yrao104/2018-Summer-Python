895265f91ff23f0dfecfb966d807c8f7d5148484
Yamuna Rao <63515445+yrao104@users.noreply.github.com>
2020-04-11 12:37:33 -0700

    Create CustomFunction.py

---
+++ Unit8/CustomFunction.py
@@ -0,0 +1,15 @@
+'''
+Yamuna Rao
+7/14
+Create your own function that processes a list. Data for the list is up to you- turtle objects,
+colors, names etc.
+'''
+
+def customlist(a, b, c, d, e, f): # the six variables/elements can be any random words, integers, or phrases
+  myList = [a, b, c, d, e, f]
+  print(myList)
+
+customlist("dog", "turtle", "arrow", "teal", 5, 6) # tests function with different types of elements
+
+  
+  
