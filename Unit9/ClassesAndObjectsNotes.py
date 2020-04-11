94aa19bc75371103553f84682671f4c1874fc9fb
Yamuna Rao <63515445+yrao104@users.noreply.github.com>
2020-04-11 12:40:09 -0700

    Update ClassesAndObjectsNotes.py

--- Unit9/ClassesAndObjectsNotes.py
+++ Unit9/ClassesAndObjectsNotes.py
@@ -52,3 +52,34 @@ circle.drawCircle(t1)
 #print the radius of that circle
 print(circle.getRadius()) #use an accessor method to get instance variable
 '''
+
+import math
+import turtle
+
+class Circle:
+  def __init__(self, r): #constructor(construct an object for the class)
+    self.radius = r #instance variable, makes every variable have a radius of 1
+    #the inital data for the circle(above)
+    #self means that it is associated with the object
+    
+  def getRadius(self): #accessor or getter methods
+    return self.radius
+    
+  def setRadius(self, r): #mutator methods or setter methods
+    self.radius = r
+    
+  def area(self): #utility methods
+    return self.radius * self.radius * math.pi
+    
+  def drawCircle(self, t):
+    t.circle(self.radius)
+
+circle1 = Circle()#finds the class and calls the constructor
+t1 = turtle.Turtle()#makes another object(turtle)
+circle1.drawCircle(t1)#utility method
+
+circle2 = Circle()
+#change circle2 radius to 100
+circle2.setRadius(100) #calls setRadius function and changes function
+#drawCircle
+circle2.drawCircle(t1)
